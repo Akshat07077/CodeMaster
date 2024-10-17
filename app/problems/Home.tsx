7c5f1dbddf51b17384d8a265469e17e9057c96b2
Akshat Sharma <sakshat988@gmail.com>
2024-10-17 17:01:09 +0530

    updates

--- app/problems/Home.tsx
+++ app/problems/Home.tsx
@@ -6,12 +6,18 @@ import Categories from "./Categories";
 import Sidebar from "./Sidebar";
 
 function Home() {
+  const [filteredCategory, setFilteredCategory] = useState<string | null>(null);
+
+  // This function will be passed to the Categories component
+  const handleFilterChange = (category: string) => {
+    setFilteredCategory(category); // Update the state with the selected category
+  };
   return (
     <div className="flex gap-0">
       <div className="bg-[#1c1c1c] h-full flex-col items-start w-[90%]">
         <ComapaniesCards />
-        <Categories />
-        <ProblemsList categoryFilter={''}/>
+        <Categories onFilterChange={handleFilterChange} />
+        <ProblemsList categoryFilter={''} />
       </div>
       <div className="mt-10">
         <div className=" mr-10">
